34914984f011b384c2e1564fdc1581c263889c19
kiranknz <kiran@kapco.co.nz>
2022-01-19 13:19:25 +1300

    Add project files.

---
+++ BooksDBConsoleApp/Models/staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksDBConsoleApp.Models
+{
+    [Table("Staff")]
+    public partial class staff
+    {
+        [Key]
+        public int StaffID { get; set; }
+        [StringLength(50)]
+        public string StaffName { get; set; } = null!;
+    }
+}
